64ee5f6c95a4d553069707b8b0cedaabe2ee3c57
Brett Beekley <brettbeekley@gmail.com>
2013-05-07 17:00:04 +0300

    Update interactions.py

--- interactions.py
+++ interactions.py
@@ -1,4 +1,12 @@
-class human():
-  money = 0
+class Human():
+	def giveItem(self,item):
+		self.inventory.append(item)
+	money = 0
 	hunger = 0
 	inventory = []
+	
+class Food():
+	filling = 50
+	
+Paul = Human()
+Banana = Food()
